99aad168939af8a616e012b99013b1012d5f3a80
GustavGroenborg <gustavrisagerus@gmail.com>
2023-11-29 16:05:55 +0100

    Finished dec 4 part 1

---
+++ dec4/src/main/java/Dec4.java
@@ -0,0 +1,48 @@
+import java.nio.file.*;
+import java.util.List;
+import java.util.Arrays;
+import org.apache.commons.lang3.Range;
+
+public class Dec4 {
+    public static void main(String[] args) {
+        String[] input = loadPuzzleInput();
+
+        System.out.println("One range fully contains the other in "
+                + Dec4.part1(input) + " " + "assignments.");
+    }
+
+    public static int part1(String[] strings) {
+        long count = Arrays.stream(strings)
+                .map(pair -> pair.split(","))
+                .map(pairs -> Arrays.stream(pairs)
+                        .map(Dec4::parseRange)
+                        .toList())
+                .filter(ranges -> (ranges.get(0).containsRange(ranges.get(1))
+                            || ranges.get(1).containsRange(ranges.get(0))))
+                .count();
+
+        return (int) count;
+    }
+
+    public static Range<Integer> parseRange(String unparsed) {
+        Integer[] nums = Arrays.stream(unparsed.split("-"))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toArray(Integer[]::new);
+
+        return Range.between(nums[0], nums[1]);
+    }
+
+    public static String[] loadPuzzleInput() {
+        String path = System.getProperty("user.home")
+                +"/AdventOfCode/aoc_2022/puzzleInputs/dec4.txt";
+
+        Path fp = Path.of(path);
+
+        try {
+            return Files.readString(fp).split("\n");
+        } catch(Exception e) {
+            throw new Error(e.getMessage());
+        }
+    }
+}
